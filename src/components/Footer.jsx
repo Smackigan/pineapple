ecbf9fa4ec4d99b22aabd8d79d48b8d5db87eeea
Paul <pablovanduo@gmail.com>
2023-10-20 17:05:33 +0300

    Social icons states

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,45 @@
-import React from 'react';
-import Social from './Social';
-// import facebook from './img/ic_facebook-1.svg';
+import Icons from './icons';
 
 function Footer() {
+	const socialLinks = [
+		{
+			icon: <Icons.Facebook />,
+			url: '#',
+			hoverColor: '#4267B2',
+			activeColor: '#2F4A80',
+		},
+		{
+			icon: <Icons.Instagram />,
+			url: '#',
+			hoverColor: '#C13584',
+			activeColor: '#8F2762',
+		},
+		{
+			icon: <Icons.Twitter />,
+			url: '#',
+			hoverColor: '#1DA1F2',
+			activeColor: '#177FBF',
+		},
+		{
+			icon: <Icons.Youtube />,
+			url: '#',
+			hoverColor: '#FF0000',
+			activeColor: '#CC0000',
+		},
+	];
+
 	return (
 		<>
 			<div className="w-[480px] h-[1px] bg-[#E3E3E4] mt-[50px] mx-[100px]"></div>
 			<div className="flex mt-[50px] mx-[218px] justify-between">
-				<Social />
-				<Social />
-				<Social />
-				<Social />
+				{socialLinks.map((link, index) => (
+					<a key={index} href={link.url}>
+						<div
+							className={`icon-container w-[46px] h-[46px] border-[2px] border-[#E2E3E4] rounded-full flex items-center justify-center hover:bg-[${link.hoverColor}] hover:border-[${link.hoverColor}] active:bg-[${link.activeColor}] active:border-[${link.activeColor}]`}>
+							{link.icon}
+						</div>
+					</a>
+				))}
 			</div>
 		</>
 	);
---
+++ src/components/icons.js
@@ -0,0 +1,13 @@
+import { ReactComponent as FacebookIcon } from '../assets/ic_facebook-1.svg';
+import { ReactComponent as InstagramIcon } from '../assets/ic_instagram-1.svg';
+import { ReactComponent as TwitterIcon } from '../assets/ic_twitter-1.svg';
+import { ReactComponent as YoutubeIcon } from '../assets/ic_youtube-1.svg';
+
+const Icons = {
+	Facebook: FacebookIcon,
+	Instagram: InstagramIcon,
+	Twitter: TwitterIcon,
+	Youtube: YoutubeIcon,
+};
+
+export default Icons;
